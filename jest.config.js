57e611e34c482f59719c2435eb35cbc5debabb8d
Faiz Ahmed <152106688+Faiz-Ahmed08@users.noreply.github.com>
2023-12-20 23:29:31 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,6 @@
-/** @type {import('jest').Config} **/
+/** @type {import('jest').Config} */
 const config = {
-testEnvironment: "allure-jest", 
-   verbose: true,
+  verbose: true,
 };
 
 module.exports = config;
-
-// allure_fixture_test.py
-//
-// allure_fullname_test.py
-// (File)
-//
-// allure_parametrized_test.py
-// (File)
-//
-// {Merge pull request #32 from skhomuti/python-fixes
-//
-// allure_status_test.py
-// (File)
-//
-// {Merge pull request #32 from skhomuti/python-fixes
-//
-// allure_title.py

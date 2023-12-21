491633e794bfda3e4235dcaa597d37d8eff251ec
Faiz Ahmed <152106688+Faiz-Ahmed08@users.noreply.github.com>
2023-12-21 23:25:20 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,4 @@
 /** @type {import('jest').Config} */
-/**const config = {
-  testEnvironment: "allure-jest", 
-   verbose: true, 
-     /**testEnvironment: "allure-jest/jsdom",*/
- /** "reporters": [
-	"default",
-	["./node_modules/jest-html-reporter", {
-		"pageTitle": "Test Report"
-	}]
-]  */
-   /**reporters: [['github-actions', {silent: false}], 'summary'],*/
-};
-
-/**module.exports = config; */
 const config = {
   testEnvironment: "allure-jest/node",
   testEnvironmentOptions: {

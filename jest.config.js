8533427e0212d1663a47c36543951b48af307726
Faiz Ahmed <152106688+Faiz-Ahmed08@users.noreply.github.com>
2023-12-21 01:15:44 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('jest').Config} */
 const config = {
-  verbose: true,
+  reporters: [
+    'default',
+    ['jest-junit', {outputDirectory: 'reports', outputName: 'report.xml'}],
+  ],
 };
 
 module.exports = config;

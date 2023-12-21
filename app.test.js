0502723ebf8d34896ab7ee225951f32543888047
Faiz Ahmed <152106688+Faiz-Ahmed08@users.noreply.github.com>
2023-12-22 00:50:04 +0100

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,5 +1,7 @@
-const sum = require("./app").sum;
+describe("Testing app sum", () => {
+  const sum = require("./app").sum;
 
 test("add two numbers", () => {
-  expect(sum(3, 5)).toBe(8);
+  expect(sum(3, 4)).toBe(8);
+  }
 });
